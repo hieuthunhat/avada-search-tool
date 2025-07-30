2867414713d1589d1cbc7de615dfb170b099dffb
hieunth <hieunguyentrong520@gmail.com>
2025-07-30 22:41:12 +0700

    remove(routes): remove unneccessary routes

--- packages/assets/src/const/navigation.js
+++ packages/assets/src/const/navigation.js
@@ -1,20 +1,20 @@
 export const navigationLinks = [
-  {
-    label: 'Samples',
-    destination: '/samples'
-  },
-  {
-    label: 'Settings',
-    destination: '/settings'
-  },
-  {
-    label: 'Tables',
-    destination: '/tables'
-  },
-  {
-    label: 'Optional Scopes',
-    destination: '/optional-scopes'
-  },
+  // {
+  //   label: 'Samples',
+  //   destination: '/samples'
+  // },
+  // {
+  //   label: 'Settings',
+  //   destination: '/settings'
+  // },
+  // {
+  //   label: 'Tables',
+  //   destination: '/tables'
+  // },
+  // {
+  //   label: 'Optional Scopes',
+  //   destination: '/optional-scopes'
+  // },
   {
     label: 'Chatting',
     destination: '/chatting'
